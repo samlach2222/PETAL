0761f90227f89472cfa6468af30409b1cb4ec58a
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-03-06 12:29:24 +0100

    Ajoute batch pour le fichier hook pre-commit
    Git gère mal les fichiers présent sur le repo qui sont inutiles à modifier mais nécessaire à avoir.
    Ce hook pre-commit permet d'enlever ces fichiers inutiles des fichiers sélectionnés à commit

---
+++ (Ré)initialiser pre-commit.bat
@@ -0,0 +1,11 @@
+::Version 1.0
+@ECHO OFF
+cd "%~dp0"
+chcp 65001
+
+echo Version 1.0
+echo.
+echo Déplace et renomme le fichier "pre-commit.hook" vers ".git\hooks\pre-commit"
+xcopy /Y /F "pre-commit.hook" ".git\hooks\pre-commit"
+echo OK
+TIMEOUT 3
