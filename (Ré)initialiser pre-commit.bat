f66dc4a6e5ae4119edd463e7834682a253f6f8b0
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-03-07 08:56:20 +0100

    Corrige destination batch pre-commit mais pour de vrai
    Corrige 2effc81f5b9eb6698c139048c779b0b85bb285fd qui copiait le fichier pre-commit.hook sans le renommer en pre-commit

--- (Ré)initialiser pre-commit.bat
+++ (Ré)initialiser pre-commit.bat
@@ -6,6 +6,6 @@ chcp 65001
 echo Version 1.0
 echo.
 echo Déplace et renomme le fichier "pre-commit.hook" vers ".git\hooks\pre-commit"
-xcopy /Y /F "pre-commit.hook" ".git\hooks\"
+xcopy /Y /F /I "pre-commit.hook" ".git\hooks\pre-commit"
 echo OK
 TIMEOUT 3
