ed419ad91ee833d30eb922255e02e561e59805a7
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-03-07 08:56:31 +0100

    pre-commit: Ne commit pas les fichiers de la BDD phpmyadmin

--- (Ré)initialiser pre-commit.bat
+++ (Ré)initialiser pre-commit.bat
@@ -1,9 +1,9 @@
-::Version 1.0
+::Version 1.1
 @ECHO OFF
 cd "%~dp0"
 chcp 65001
 
-echo Version 1.0
+echo Version 1.1
 echo.
 echo Déplace et renomme le fichier "pre-commit.hook" vers ".git\hooks\pre-commit"
 xcopy /Y /F /I "pre-commit.hook" ".git\hooks\pre-commit"
