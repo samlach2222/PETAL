593a9c6379e793b4ad1d0dfb03d13e5b49b17a89
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-03-09 17:05:32 +0100

    Optimise légèrement hook pre-commit

--- (Ré)initialiser pre-commit.bat
+++ (Ré)initialiser pre-commit.bat
@@ -1,9 +1,9 @@
-::Version 1.1
+::Version 1.2
 @ECHO OFF
 cd "%~dp0"
 chcp 65001
 
-echo Version 1.1
+echo Version 1.2
 echo.
 echo Déplace et renomme le fichier "pre-commit.hook" vers ".git\hooks\pre-commit"
 copy "pre-commit.hook" ".git\hooks\"
