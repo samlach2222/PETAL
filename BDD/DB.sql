b435a9fe0307adf51e95eb6bb14ffff9b94f6179
samlach2222 <lachaudsamuel@protonmail.com>
2022-03-09 15:52:44 +0100

    Mise à jour Conception BDD et ajout du fichier de DB

---
+++ BDD/DB.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS PETAL_DB;
+USE PETAL_DB;
+
+-- Exemple de création de table
+/*CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);*/
