6d3030989828392e2fe5eabb70800301c25329ec
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-03-11 09:20:18 +0100

    pre-commit améliore visibilitée erreurs

--- (Ré)initialiser pre-commit.bat
+++ (Ré)initialiser pre-commit.bat
@@ -1,9 +1,9 @@
-::Version 1.3
+::Version 1.4
 @ECHO OFF
 cd "%~dp0"
 chcp 65001
 
-echo Version 1.3
+echo Version 1.4
 echo.
 echo Déplace et renomme le fichier "pre-commit.hook" vers ".git\hooks\pre-commit"
 copy "pre-commit.hook" ".git\hooks\"
