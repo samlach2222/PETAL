967c6c9e7ed001abe52717da1a54b046546588ab
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-03-11 08:43:33 +0100

    pre-commit enlève d'autres fichiers perso et sépare fichiers BDD/Web

--- (Ré)initialiser pre-commit.bat
+++ (Ré)initialiser pre-commit.bat
@@ -1,9 +1,9 @@
-::Version 1.2
+::Version 1.3
 @ECHO OFF
 cd "%~dp0"
 chcp 65001
 
-echo Version 1.2
+echo Version 1.3
 echo.
 echo Déplace et renomme le fichier "pre-commit.hook" vers ".git\hooks\pre-commit"
 copy "pre-commit.hook" ".git\hooks\"
