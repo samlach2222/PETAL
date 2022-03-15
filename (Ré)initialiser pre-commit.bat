f436d1d95be4e7f4514ccb1d15fae332034d6464
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-03-16 00:17:42 +0100

    pre-commit enlève les fichiers uwamp*CommitableFiles.tmp

--- (Ré)initialiser pre-commit.bat
+++ (Ré)initialiser pre-commit.bat
@@ -1,9 +1,9 @@
-::Version 3.0
+::Version 4.0
 @ECHO OFF
 cd "%~dp0"
 chcp 65001
 
-echo Version 3.0
+echo Version 4.0
 echo.
 echo Déplace et renomme le fichier "pre-commit.hook" vers ".git\hooks\pre-commit"
 copy "pre-commit.hook" ".git\hooks\"
