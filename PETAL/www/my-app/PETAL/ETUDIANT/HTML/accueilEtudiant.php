788b2556b18ccfde9a5bda45546d55ae799fa592
DorianBarsoni <dbarsoni@gmail.com>
2022-03-19 12:42:29 +0100

    Ajout page accueil étudiant

---
+++ PETAL/www/my-app/PETAL/ETUDIANT/HTML/accueilEtudiant.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="../../ALL/CSS/bandeau.css">
+        <link rel="stylesheet" href="../CSS/accueil_etudiant.css">
+        <title>Page d'Accueil</title>
+    </head>
+    <body>
+        <h1>Recommandation de Cours :</h1>
+
+        <table id="centre">
+            <tr>
+                <td class="espace">
+
+                    <table class="matiere">
+                        <tr>
+                            <th>
+                                <span>BDD</span>
+                            </th>
+                        </tr>
+                        <tr>
+                            <td class="image">
+                                Image 1
+                            </td>
+                        </tr>
+                        
+                    </table>
+
+                </td>
+                <td class="espace">
+
+                    <table class="matiere">
+                        <tr>
+                            <th>
+                                <span>CDAA</span>
+                            </th>
+                        </tr>
+                        <tr>
+                            <td class="image">
+                                Image 2
+                            </td>
+                        </tr>
+                    </table>
+
+                </td>
+                <td class="espace">
+
+                    <table class="matiere">
+                        <tr>
+                            <th>
+                                <span>Modélisation</span>
+                            </th>
+                        </tr>
+                        <tr>
+                            <td class="image">
+                                Image 3
+                            </td>
+                        </tr>
+                    </table>
+
+                </td>
+            </tr>
+
+            <tr>
+                <td class="espace">
+
+                    <table class="matiere">
+                        <tr>
+                            <th>
+                                <span>Synthèse d'Images</span>
+                            </th>
+                        </tr>
+                        <tr>
+                            <td class="image">
+                                Image 4
+                            </td>
+                        </tr>
+                    </table>
+
+                </td>
+                <td class="espace">
+
+                    <table class="matiere">
+                        <tr>
+                            <th>
+                                <span>Systèmes & Réseaux</span>
+                            </th>
+                        </tr>
+                        <tr>
+                            <td class="image">
+                                Image 5
+                            </td>
+                        </tr>
+                    </table>
+
+                </td>
+                <td class="espace">
+
+                    <table class="matiere">
+                        <tr>
+                            <th>
+                                <span>Maths</span>
+                            </th>
+                        </tr>
+                        <tr>
+                            <td class="image">
+                                Image 6
+                            </td>
+                        </tr>
+                    </table>
+
+                </td>
+            </tr>
+
+        </table>
+
+
+
+        <?php include("../../ALL/HTML/bandeau.html");?>
+    </body>
+</html>
