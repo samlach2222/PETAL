18d65a444310a7768427b7fce80b6b8c1dbdb801
samlach2222 <lachaudsamuel@protonmail.com>
2022-03-22 17:10:28 +0100

    Ajout des icones light et du css associé pour gestion_utilisateurs

--- PETAL/www/my-app/PETAL/ADMINISTRATEUR/HTML/gestion_utilisateurs.php
+++ PETAL/www/my-app/PETAL/ADMINISTRATEUR/HTML/gestion_utilisateurs.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <link rel="stylesheet" href="../../ALL/CSS/bandeau.css">
-    <link rel="stylesheet" href="../CSS/gestion_utilisateurs.css">
+    <link rel="stylesheet" href="../CSS/gestion_utilisateurs_light.css">
     <link rel="icon" type="image/x-icon" href="../../Ressources/Icon/logo%20PETAL.svg">
     <meta charset="UTF-8">
     <title>Gestion des utilisateurs</title>
@@ -11,22 +10,24 @@
 <body>
     <?php include("../../ALL/HTML/bandeau.html");?>
     <div id="content">
-        <h1>Gestion des utilisateurs</h1>
+        <div id="title">
+            <h1>Gestion des utilisateurs</h1>
+        </div>
         <div id="toolbar">
             <div id="conteneurBoutonRetour">
                 <a href="accueil_admin.php" id="boutonRetour">retour</a>
             </div>
             <a href="edition_admin.php">
-                <img src="../../Ressources/Pictures/Plus_Administrateur_Dark.png" class="icon">
+                <img id="plusAdmin" src="../../Ressources/Pictures/Plus_Administrateur_Dark.png" class="icon">
             </a>
             <a href="edition_etudiant.php">
-                <img src="../../Ressources/Pictures/Plus_Utilisateur_Dark.png" class="icon">
+                <img id="plusEtudiant" src="../../Ressources/Pictures/Plus_Utilisateur_Dark.png" class="icon">
             </a>
             <a href="edition_etudiant.php">
-                <img src="../../Ressources/Pictures/Crayon_Dark.png" class="icon">
+                <img id="crayon" src="../../Ressources/Pictures/Crayon_Dark.png" class="icon">
             </a>
             <a href="">
-                <img src="../../Ressources/Pictures/Corbeille_Dark.png" class="icon">
+                <img id="corbeille" src="../../Ressources/Pictures/Corbeille_Dark.png" class="icon">
             </a>
         </div>
         <div id="liste">
