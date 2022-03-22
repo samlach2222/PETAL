525e29ee3d6fa7327d5d00aedaf152c636f93b79
Orlane_TISSERAND <orlane.tisserand71@gmail.com>
2022-03-22 15:11:16 +0100

    ajout de pages et d'images

--- PETAL/www/my-app/PETAL/ADMINISTRATEUR/HTML/edition_qcm.php
+++ PETAL/www/my-app/PETAL/ADMINISTRATEUR/HTML/edition_qcm.php
@@ -7,7 +7,43 @@
     <meta charset="UTF-8">
     <title>Edition d'un QCM</title>
 </head>
-<body>
+<body class="dark">
     <?php include("../../ALL/HTML/bandeau.html");?>
+    <div>
+    	<a href="acceuil_admin.php" id="retour"><-retour</a>
+    	<form>
+    		<table>
+    			<tr>
+    				<td>
+    					<label>Nom</label>
+    					<input type="text" name="nom">
+    				</td>
+    				<td>
+    					<label>Matière</label>
+    					<input type="text" name="matiere">
+    				</td>
+    				<td>
+    					<label>Date/heure de fin</label>
+    					<input type="date" name="dateHeureFin">
+    				</td>
+    			</tr>
+    		</table>
+    		<hr>
+    		<div>
+    			<label>Question </label>
+    			<output>1</output>
+    			<label> : </label>
+    			<input type="text" name="question"><br>
+    			<input type="radio" name="reponse1Br">
+    				<input type="text" name="reponse1"><br>
+    			<input type="radio" name="reponse1Br">
+    				<input type="text" name="reponse2"><br>
+    			<input type="radio" name="reponse1Br">
+    				<input type="text" name="reponse3"><br>
+    		</div>
+    		<button onclick="ajoutQuestion()">+</button>
+    		<button onclick="validation()">Valider</button>
+    	</form>
+    </div>
 </body>
 </html>
--- PETAL/www/my-app/PETAL/ADMINISTRATEUR/HTML/liste_qcm.php
+++ PETAL/www/my-app/PETAL/ADMINISTRATEUR/HTML/liste_qcm.php
@@ -7,7 +7,28 @@
     <meta charset="UTF-8">
     <title>Liste des QCM</title>
 </head>
-<body>
+<body class="dark">
+	<!-- faire les themes -->
     <?php include("../../ALL/HTML/bandeau.html");?>
+    <div id="corps">
+    	<!-- voir le bandeau -->
+    	<h1 class="bandeauSecondaireLight">Liste des QCM</h1>
+    	<div class="bandeauSecondaireLight" id="bandeau3">
+    		<a href="acceuil_admin.php" id="retour"><-retour</a>
+    		<div>
+    			<img src="../../Ressources/Pictures/Plus_Dark.png" class="imageIcon">
+    			<img src="../../Ressources/Pictures/Crayon_Dark.png" class="imageIcon">
+    			<img src="../../Ressources/Pictures/Corbeille_Dark.png" class="imageIcon">
+    		</div>
+    	</div>
+    	<ul>
+    		<!-- faire les li -->
+    		<li>
+    			<input type="checkbox" name="QCM1">
+    			<span>qcm1</span>
+    			<img src="../../Ressources/Pictures/Eye_Dark.png" id="eyeIcon">
+    		</li>
+    	</ul>
+    </div>
 </body>
 </html>
