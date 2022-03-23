865b4eaca765af7e9b8ea92e987a5fe51ba2194a
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-03-23 21:20:13 +0100

    pre-commit supporte fichiers renommés

--- (Ré)initialiser pre-commit.bat
+++ (Ré)initialiser pre-commit.bat
@@ -1,9 +1,9 @@
-::Version 4.1
+::Version 4.2
 @ECHO OFF
 cd "%~dp0"
 chcp 65001
 
-echo Version 4.1
+echo Version 4.2
 echo.
 echo Déplace et renomme le fichier "pre-commit.hook" vers ".git\hooks\pre-commit"
 copy "pre-commit.hook" ".git\hooks\"
