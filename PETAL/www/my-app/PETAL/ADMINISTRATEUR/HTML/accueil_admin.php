66782f5013ef45c77fa596b47fd276c43cb926eb
BetulDSENER <betul.sener@laposte.net>
2022-03-23 05:47:16 +0100

    accueil_admin

--- PETAL/www/my-app/PETAL/ADMINISTRATEUR/HTML/accueil_admin.php
+++ PETAL/www/my-app/PETAL/ADMINISTRATEUR/HTML/accueil_admin.php
@@ -9,5 +9,54 @@
 </head>
 <body>
     <?php include("../../ALL/HTML/bandeau.html");?>
+    <div id="PageAccueil">
+    <span id="retour"><- Retour</span>
+    <table id="centre">
+        <tr>
+            <td class="espace">
+                <table class="gestion">
+                    <tr>
+                        <th>
+                            <span class="titre">Gestion des cours</span>
+                        </th>
+                    </tr>
+                    <tr>
+                        <td class="image">
+                            Image
+                        </td>
+                    </tr>
+                </table>
+            </td>
+            <td class="espace">
+                <table class="gestion">
+                    <tr>
+                        <th>
+                            <span class="titre">Gestion des QCM</span>
+                        </th>
+                    </tr>
+                    <tr>
+                        <td class="image">
+                            Image
+                        </td>
+                    </tr>
+                </table>
+            </td>
+            <td class="espace">
+                <table class="gestion">
+                    <tr>
+                        <th>
+                            <span class="titre">Gestion des utilisateurs</span>
+                        </th>
+                    </tr>
+                    <tr>
+                        <td class="image">
+                            Image
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</div>
 </body>
 </html>
