445d084a6e91c0491358fbc812a7121bc4c3d46b
BetulDSENER <betul.sener@laposte.net>
2022-03-30 00:19:56 +0200

    edition_cours

--- PETAL/www/my-app/PETAL/ADMINISTRATEUR/HTML/edition_cours.php
+++ PETAL/www/my-app/PETAL/ADMINISTRATEUR/HTML/edition_cours.php
@@ -1,79 +1,58 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <link rel="stylesheet" href="../CSS/edition_cours.css">
+    <link rel="stylesheet" href="../CSS/edition_cours_light.css">
     <link rel="icon" type="image/x-icon" href="../../Ressources/Icon/logo%20PETAL.svg">
     <meta charset="UTF-8">
     <title>Edition d'un cours</title>
 </head>
-
 <body>
-    <?php include("../../ALL/HTML/bandeau.html");?>
-    <div id="content">
-        <div id="title">
-            <h1>Edition matiere </h1>
-        </div>
-        <div id="toolbar">
-            <div id="conteneurBoutonRetour">
-                <a href="accueil_admin.php" id="boutonRetour">retour</a>
-            </div>
-            
-            <a href="edition_etudiant.php">
-                <div id="plus" class="icon">+</div>
-            </a>
-            <a href="edition_etudiant.php">
-                <img id="crayon" src="../../Ressources/Pictures/Crayon_Dark.png" class="icon">
-            </a>
-            <a href="">
-                <img id="corbeille" src="../../Ressources/Pictures/Corbeille_Dark.png" class="icon">
-            </a>
-        </div>
-        <div id="liste">
-            <table>
-                <tr>
-                    <td>
-                        <label>
-                            <input type="checkbox" name="key" value="value" />
-                            <span>Cours1.pdf</span>
-                        </label>
-                    </td>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label>
-                            <input type="checkbox" name="key" value="value" />
-                            <span>Cours2.zip</span>
-                        </label>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label>
-                            <input type="checkbox" name="key" value="value" />
-                            <span>CM3.pptx</span>
-                        </label>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label>
-                            <input type="checkbox" name="key" value="value" />
-                            <span>CM6.xlsx</span>
-                        </label>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label>
-                            <input type="checkbox" name="key" value="value" />
-                            <span>...</span>
-                        </label>
-                    </td>
-                </tr>
-            </table>
-        </div>
+<?php include("../../ALL/HTML/bandeau.html");?>
+<div id="content">
+    <div id="title">
+        <h1 id="createAdmin">Ajout de cours</h1>
     </div>
+    <form>
+        <table>
+            <tr>
+                <td>
+                    <label for="titreCours">Titre du cours</label></br>
+                    <input type="text" id="titreCours"/>
+                </td>
+                <td rowspan="3">
+                    <label>Fichier</label></br>
+                    <input type="button" id="ajoutFichier" value=""/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="typeCours">Type de cours</label></br>
+                    <select name="typeCours" id="typeCours">
+                        <option value="CM">CM</option>
+                        <option value="TD">TD</option>
+                        <option value="TP">TP</option>
+                    </select>
+                </td>
+                
+            </tr>
+            <tr>
+                <td>
+                    <label for="nomProf">Nom du professeur</label></br>
+                    <input type="text" id="nomProf"/>
+                </td>
+                
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit" value="Valider" id="valider">
+                </td>
+                <td>
+                    <input type="button" value="Annuler" id="annuler" onClick="window.location.href='gestion_cours.php'">
+                </td>
+            </tr>
+        </table>
+    </form>
+</div>
 
 </body>
 </html>
