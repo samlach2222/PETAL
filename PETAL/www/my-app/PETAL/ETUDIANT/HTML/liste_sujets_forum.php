5148992ba1252df23b25b5249fd64d803f230f95
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-03-29 15:12:17 +0200

    Ajoute liste_sujets_forum dark theme

--- PETAL/www/my-app/PETAL/ETUDIANT/HTML/liste_sujets_forum.php
+++ PETAL/www/my-app/PETAL/ETUDIANT/HTML/liste_sujets_forum.php
@@ -1,13 +1,70 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <link rel="stylesheet" href="../../ALL/CSS/bandeau.css">
-    <link rel="stylesheet" href="../CSS/liste_sujets_forum.css">
+    <link rel="stylesheet" href="../CSS/liste_sujets_forum_dark.css">
     <link rel="icon" type="image/x-icon" href="../../Ressources/Icon/logo%20PETAL.svg">
     <meta charset="UTF-8">
     <title>Liste des sujet du forum</title>
 </head>
 <body>
     <?php include("../../ALL/HTML/bandeau.html");?>
+    <div id="content">
+        <div id="gauche-cours">
+            <ul>
+                <li>CDAA</li>
+                <li>SR</li>
+                <li>SI</li>
+                <li>Maths</li>
+                <li>BDD</li>
+                <li>Modélisation</li>
+            </ul>
+        </div>
+        <div id="droite-sujets">
+            <table>
+                <tr>
+                    <th>Sujet</th>
+                    <th>Lancé par</th>
+                    <th>Nombre de réponses</th>
+                    <th>Résolu</th>
+                </tr>
+                <tr>
+                    <td>Sujet 1</td>
+                    <td>NOM Prénom</td>
+                    <td>3</td>
+                    <td><img src="../../Ressources/Pictures/non_résolu.png" width="40px" height="40px"/></td>
+                </tr>
+                <tr>
+                    <td>Sujet 2</td>
+                    <td>NOM Prénom</td>
+                    <td>7</td>
+                    <td><img src="../../Ressources/Pictures/résolu.png" width="40px" height="40px"/></td>
+                </tr>
+                <tr>
+                    <td>Sujet 3</td>
+                    <td>NOM Prénom</td>
+                    <td>2</td>
+                    <td><img src="../../Ressources/Pictures/non_résolu.png" width="40px" height="40px"/></td>
+                </tr>
+                <tr>
+                    <td>Sujet 4</td>
+                    <td>NOM Prénom</td>
+                    <td>5</td>
+                    <td><img src="../../Ressources/Pictures/résolu.png" width="40px" height="40px"/></td>
+                </tr>
+                <tr>
+                    <td>Sujet 5</td>
+                    <td>NOM Prénom</td>
+                    <td>2</td>
+                    <td><img src="../../Ressources/Pictures/non_résolu.png" width="40px" height="40px"/></td>
+                </tr>
+                <tr>
+                    <td>Sujet 6</td>
+                    <td>NOM Prénom</td>
+                    <td>0</td>
+                    <td><img src="../../Ressources/Pictures/non_résolu.png" width="40px" height="40px"/></td>
+                </tr>
+            </table>
+        </div>
+    </div>
 </body>
 </html>
