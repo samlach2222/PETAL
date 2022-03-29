9ee63c002403b07894e3aa4d6e3a05447b592922
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-03-29 23:40:39 +0200

    Ajoute liste_sujets_forum light theme

--- PETAL/www/my-app/PETAL/ETUDIANT/HTML/liste_sujets_forum.php
+++ PETAL/www/my-app/PETAL/ETUDIANT/HTML/liste_sujets_forum.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <link rel="stylesheet" href="../CSS/liste_sujets_forum_dark.css">
-    <link rel="icon" type="image/x-icon" href="../../Ressources/Icon/logo%20PETAL.svg">
+    <link rel="icon" type="image/x-icon" href="../../Ressources/Icon/logo PETAL.svg">
     <meta charset="UTF-8">
     <title>Liste des sujet du forum</title>
 </head>
