f9b277094d4764dbd84992645e6e2389b81af0e9
BetulDSENER <betul.sener@laposte.net>
2022-03-30 08:44:35 +0200

    accueil_admin

--- PETAL/www/my-app/PETAL/ADMINISTRATEUR/HTML/accueil_admin.php
+++ PETAL/www/my-app/PETAL/ADMINISTRATEUR/HTML/accueil_admin.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <link rel="stylesheet" href="../../ALL/CSS/bandeau.css">
-    <link rel="stylesheet" href="../CSS/accueil_admin_dark.css">
+    <link rel="stylesheet" href="../CSS/accueil_admin_light.css">
     <link rel="icon" type="image/x-icon" href="../../Ressources/Icon/logo%20PETAL.svg">
     <meta charset="UTF-8">
     <title>Page d'accueil</title>
@@ -13,46 +13,52 @@
         <table id="centre">
             <tr>
                 <td class="espace">
-                    <table class="gestion">
-                        <tr>
-                            <th>
-                                <span class="titre">Gestion des cours</span>
-                            </th>
-                        </tr>
-                        <tr>
-                            <td class="image">
-                                Image
-                            </td>
-                        </tr>
-                    </table>
+                    <a href="gestion_matiere.php">
+                        <table id="gestionMatiere" class="gestion">
+                            <tr>
+                                <th>
+                                    <span class="titre">Gestion des matières</span>
+                                </th>
+                            </tr>
+                            <tr>
+                                <td class="image">
+                                    Image
+                                </td>
+                            </tr>
+                        </table>
+                    </a>
                 </td>
                 <td class="espace">
-                    <table class="gestion">
-                        <tr>
-                            <th>
-                                <span class="titre">Gestion des QCM</span>
-                            </th>
-                        </tr>
-                        <tr>
-                            <td class="image">
-                                Image
-                            </td>
-                        </tr>
-                    </table>
+                    <a href="liste_qcm.php">
+                        <table id="gestionQCM" class="gestion">
+                            <tr>
+                                <th>
+                                    <span class="titre">Gestion des QCM</span>
+                                </th>
+                            </tr>
+                            <tr>
+                                <td class="image">
+                                    Image
+                                </td>
+                            </tr>
+                        </table>
+                    </a>
                 </td>
                 <td class="espace">
-                    <table class="gestion">
-                        <tr>
-                            <th>
-                                <span class="titre">Gestion des utilisateurs</span>
-                            </th>
-                        </tr>
-                        <tr>
-                            <td class="image">
-                                Image
-                            </td>
-                        </tr>
-                    </table>
+                    <a href="gestion_utilisateurs.php">
+                        <table id="gestionUtilisateurs" class="gestion">
+                            <tr>
+                                <th>
+                                    <span class="titre">Gestion des utilisateurs</span>
+                                </th>
+                            </tr>
+                            <tr>
+                                <td class="image">
+                                    Image
+                                </td>
+                            </tr>
+                        </table>
+                    </a>
                 </td>
             </tr>
         </table>
