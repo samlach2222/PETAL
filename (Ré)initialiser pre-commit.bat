bdf95550824cc420776933158c6be8cec6e713df
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-03-31 18:10:41 +0200

    pre-commit ne commit pas BDD si seulement ibdata1 selectionné

--- (Ré)initialiser pre-commit.bat
+++ (Ré)initialiser pre-commit.bat
@@ -1,9 +1,9 @@
-::Version 4.2
+::Version 4.3
 @ECHO OFF
 cd "%~dp0"
 chcp 65001
 
-echo Version 4.2
+echo Version 4.3
 echo.
 echo Déplace et renomme le fichier "pre-commit.hook" vers ".git\hooks\pre-commit"
 copy "pre-commit.hook" ".git\hooks\"
