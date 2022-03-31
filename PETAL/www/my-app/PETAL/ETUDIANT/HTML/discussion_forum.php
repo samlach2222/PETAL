791ad4ef2999d82d97e5f00ec7fecccc3da617f8
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-03-31 08:41:47 +0200

    Début de discussion_forum

--- PETAL/www/my-app/PETAL/ETUDIANT/HTML/discussion_forum.php
+++ PETAL/www/my-app/PETAL/ETUDIANT/HTML/discussion_forum.php
@@ -1,13 +1,40 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <link rel="stylesheet" href="../../ALL/CSS/bandeau.css">
-    <link rel="stylesheet" href="../CSS/discussion_forum.css">
-    <link rel="icon" type="image/x-icon" href="../../Ressources/Icon/logo%20PETAL.svg">
+    <link rel="stylesheet" href="../CSS/discussion_forum_dark.css">
+    <link rel="icon" type="image/x-icon" href="../../Ressources/Icon/logo PETAL.svg">
     <meta charset="UTF-8">
     <title>Discussions du forum</title>
 </head>
 <body>
     <?php include("../../ALL/HTML/bandeau.html");?>
+    <div id="content">
+        <div id="gauche-cours">
+            <ul>
+                <li>CDAA</li>
+                <li>SR</li>
+                <li>SI</li>
+                <li>Maths</li>
+                <li>BDD</li>
+                <li>Modélisation</li>
+            </ul>
+        </div>
+        <div id="droite-sujet">
+            <div id="bandeau-sujet">
+                <div id="bandeau-sujet-resolu">
+                    <span>Résolu : </span>
+                    <img src="../../Ressources/Pictures/résolu.png" width="40px" height="40px"/>
+                    <span> / </span>
+                    <img src="../../Ressources/Pictures/non_résolu.png" width="40px" height="40px"/>
+                </div>
+                <div id="bandeau-sujet-nom">
+                    Sujet 1
+                </div>
+            </div>
+            <div id="messages">
+                a
+            </div>
+        </div>
+    </div>
 </body>
 </html>
