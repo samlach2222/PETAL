4026844808e8b314d7f1d1a1bbf53f2812d7fa74
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-04-04 15:38:59 +0200

    pre-commit réaffiche ibdata1 si la BDD est commit

--- (Ré)initialiser pre-commit.bat
+++ (Ré)initialiser pre-commit.bat
@@ -1,9 +1,9 @@
-::Version 4.3
+::Version 4.4
 @ECHO OFF
 cd "%~dp0"
 chcp 65001
 
-echo Version 4.3
+echo Version 4.4
 echo.
 echo Déplace et renomme le fichier "pre-commit.hook" vers ".git\hooks\pre-commit"
 copy "pre-commit.hook" ".git\hooks\"
