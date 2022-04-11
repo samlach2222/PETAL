e9aaeb7fe0547ec3db159346daf5721da5034b73
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-04-11 15:20:41 +0200

    pre-commit affiche et recache ibdata1 si caché

--- (Ré)initialiser pre-commit.bat
+++ (Ré)initialiser pre-commit.bat
@@ -1,9 +1,9 @@
-::Version 4.4
+::Version 4.5
 @ECHO OFF
 cd "%~dp0"
 chcp 65001
 
-echo Version 4.4
+echo Version 4.5
 echo.
 echo Déplace et renomme le fichier "pre-commit.hook" vers ".git\hooks\pre-commit"
 copy "pre-commit.hook" ".git\hooks\"
