85dd1a8dace43bb6df8f6ffcf9844ab8c106bb86
ZHAlHalabi <92623259+ZHAlHalabi@users.noreply.github.com>
2022-04-12 17:00:26 +0200

    Modification Maquette

---
+++ PETAL/www/my-app/PETAL/ALL/HTML/model_connexion.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+    include('connexion.html');
+
+    try
+    {
+        $conn = new PDO('mysql:host=localhost;dbname=petal_db','root', 'root');    
+    }
+    catch(Exception $e)
+    {
+            die('Erreur : '.$e->getMessage());
+    }
+    
+
+    if(isset($_POST['nom_user']) && isset($_POST['mdp']))
+    {
+        //echo $_POST['nom_user']." ".$_POST['mdp'];
+        $_SESSION['nom_user'] = $_POST['nom_user'];
+        $_SESSION['mdp'] = $_POST['mdp'];
+    }
+?>
+
