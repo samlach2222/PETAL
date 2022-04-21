fd658c1a28e4c8c5e7805a3022ad5800cadd21f5
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-04-21 20:16:30 +0200

    Enlève tables Etudiant et Administrateur
    Triggers pour la table Matiere uniquement, il manque les autres tables

--- BDD/PETAL_DB.sql
+++ BDD/PETAL_DB.sql
@@ -5,8 +5,6 @@ USE PETAL_DB;
 
 -- Destruction des tables si existantes
 DROP TABLE IF EXISTS Utilisateur;
-DROP TABLE IF EXISTS Administrateur;
-DROP TABLE IF EXISTS Etudiant;
 DROP TABLE IF EXISTS Matiere;
 DROP TABLE IF EXISTS SujetForum;
 DROP TABLE IF EXISTS MessageForum;
@@ -19,6 +17,8 @@ DROP TABLE IF EXISTS ReponseDeEtudiant;
 -- Creation des tables
 CREATE TABLE Utilisateur (
     id INT NOT NULL AUTO_INCREMENT,
+    num INT NOT NULL,
+    admin BOOLEAN NOT NULL DEFAULT false,
     photoProfil LONGBLOB,
     nom VARCHAR(50) NOT NULL,
     prenom VARCHAR(50) NOT NULL,
@@ -28,34 +28,20 @@ CREATE TABLE Utilisateur (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE Administrateur (
-    idAdmin INT NOT NULL,
-    numAdministrateur INT NOT NULL,
-    PRIMARY KEY (idAdmin),
-    FOREIGN KEY (idAdmin) REFERENCES Utilisateur(id) ON DELETE CASCADE
-);
-
-CREATE TABLE Etudiant (
-    idEtu INT NOT NULL,
-    numEtudiant INT NOT NULL,
-    PRIMARY KEY (idEtu),
-    FOREIGN KEY (idEtu) REFERENCES Utilisateur(id) ON DELETE CASCADE
-);
-
 CREATE TABLE Matiere (
     nomMatiere VARCHAR(50) NOT NULL,
     image LONGBLOB,
-    idAdmin INT NOT NULL,
+    id INT NOT NULL,
     PRIMARY KEY(nomMatiere),
-    FOREIGN KEY(idAdmin) REFERENCES Administrateur(idAdmin) ON DELETE CASCADE
+    FOREIGN KEY(id) REFERENCES Utilisateur(id) ON DELETE CASCADE    
 );
 
 CREATE TABLE EtuMatiere (
-    idEtu INT NOT NULL,
+    id INT NOT NULL,
     nomMatiere VARCHAR(50) NOT NULL,
-    PRIMARY KEY (idEtu, nomMatiere),
-    FOREIGN KEY (idEtu) REFERENCES Etudiant(idEtu) ON DELETE CASCADE,
-    FOREIGN KEY (nomMatiere) REFERENCES Matiere(nomMatiere) ON DELETE CASCADE
+    PRIMARY KEY (id, nomMatiere),
+    FOREIGN KEY (id) REFERENCES Utilisateur(id) ON DELETE CASCADE,
+    FOREIGN KEY (nomMatiere) REFERENCES Matiere(nomMatiere) ON DELETE CASCADE    
 );
 
 CREATE TABLE SujetForum (
@@ -63,10 +49,10 @@ CREATE TABLE SujetForum (
     nomSujet VARCHAR(50) NOT NULL,
     resolu BOOLEAN NOT NULL DEFAULT false,
     nomMatiere VARCHAR(50) NOT NULL,
-    idEtu INT NOT NULL,
+    id INT NOT NULL,
     PRIMARY KEY (idSujetForum),
     FOREIGN KEY (nomMatiere) REFERENCES Matiere(nomMatiere) ON DELETE CASCADE,
-    FOREIGN KEY (idEtu) REFERENCES Etudiant(idEtu) ON DELETE CASCADE
+    FOREIGN KEY (id) REFERENCES Utilisateur(id) ON DELETE CASCADE    
 );
 
 CREATE TABLE MessageForum (
@@ -74,10 +60,10 @@ CREATE TABLE MessageForum (
     contenuMessage VARCHAR(2000) NOT NULL,
     dateHeure DATETIME NOT NULL,
     idSujetForum INT NOT NULL,
-    idEtu INT NOT NULL,
+    id INT NOT NULL,
     PRIMARY KEY (idMessage),
     FOREIGN KEY (idSujetForum) REFERENCES SujetForum(idSujetForum) ON DELETE CASCADE,
-    FOREIGN KEY (idEtu) REFERENCES Etudiant(idEtu) ON DELETE CASCADE
+    FOREIGN KEY (id) REFERENCES Utilisateur(id) ON DELETE CASCADE    
 );
 
 CREATE TABLE Cours (
@@ -102,12 +88,12 @@ CREATE TABLE QCM (
 );
 
 CREATE TABLE ResultatEtudiant (
-    idEtu INT NOT NULL,
+    id INT NOT NULL,
     idQCM INT NOT NULL,
     noteExamen DECIMAL(4,2),
-    PRIMARY KEY(idEtu, idQCM),
-    FOREIGN KEY(idEtu) REFERENCES Etudiant(idEtu) ON DELETE CASCADE,
-    FOREIGN KEY(idQCM) REFERENCES QCM(idQCM) ON DELETE CASCADE
+    PRIMARY KEY(id, idQCM),
+    FOREIGN KEY(id) REFERENCES Utilisateur(id) ON DELETE CASCADE,
+    FOREIGN KEY(idQCM) REFERENCES QCM(idQCM) ON DELETE CASCADE    
 );
 
 CREATE TABLE Question (
@@ -121,21 +107,52 @@ CREATE TABLE Question (
 );
 
 CREATE TABLE ReponseDeEtudiant (
-    idEtu INT NOT NULL,
+    id INT NOT NULL,
     idQuestion INT NOT NULL,
     reponseChoisie VARCHAR(7),
     reponseJuste BOOLEAN NOT NULL,
-    PRIMARY KEY (idEtu, idQuestion),
-    FOREIGN KEY (idEtu) REFERENCES Etudiant(idEtu) ON DELETE CASCADE,
-    FOREIGN KEY (idQuestion) REFERENCES Question(idQuestion) ON DELETE CASCADE
+    PRIMARY KEY (id, idQuestion),
+    FOREIGN KEY (id) REFERENCES Utilisateur(id) ON DELETE CASCADE,
+    FOREIGN KEY (idQuestion) REFERENCES Question(idQuestion) ON DELETE CASCADE    
 );
 
 CREATE VIEW MoyenneEtuMatiere AS
-    SELECT ResultatEtudiant.idEtu, nom, prenom, ROUND(SUM(noteExamen)/COUNT(noteExamen), 2) AS moyenne, nomMatiere
-    FROM QCM NATURAL JOIN ResultatEtudiant LEFT JOIN Etudiant ON ResultatEtudiant.idEtu = Etudiant.idEtu LEFT JOIN Utilisateur ON Etudiant.idEtu = Utilisateur.id
-    GROUP BY idEtu;
+    SELECT ResultatEtudiant.id, nom, prenom, ROUND(SUM(noteExamen)/COUNT(noteExamen), 2) AS moyenne, nomMatiere
+    FROM QCM NATURAL JOIN ResultatEtudiant LEFT JOIN Utilisateur ON ResultatEtudiant.id = Utilisateur.id
+    GROUP BY id;
 
 CREATE VIEW MoyenneQCM AS
     SELECT idQCM, nomQCM, ROUND(SUM(noteExamen)/COUNT(noteExamen), 2) AS moyenne, nomMatiere
     FROM QCM NATURAL JOIN ResultatEtudiant
     GROUP BY idQCM;
+    
+    
+-- Initialisation de la fonction IsAdmin
+DROP FUNCTION IF EXISTS IsAdmin;
+DELIMITER $$
+CREATE FUNCTION IsAdmin(p_id INT) RETURNS tinyint(1)
+BEGIN
+    RETURN (SELECT admin FROM Utilisateur WHERE id = p_id);
+END$$
+DELIMITER ;    
+
+-- Creation des triggers pour vérifier que l'utilisateur est ou n'est pas un admin
+DROP TRIGGER IF EXISTS trigger_matiere_admin_insert;
+delimiter $$
+CREATE TRIGGER trigger_matiere_admin_insert BEFORE INSERT
+ON matiere
+FOR EACH ROW
+IF IsAdmin(NEW.id) != 1 THEN
+    SIGNAL SQLSTATE '50001' SET MESSAGE_TEXT = 'Un étudiant ne peut pas gérer une matière';
+END IF; $$
+delimiter ;
+
+DROP TRIGGER IF EXISTS trigger_matiere_admin_insert;
+delimiter $$
+CREATE TRIGGER trigger_matiere_admin_insert BEFORE UPDATE
+ON matiere
+FOR EACH ROW
+IF IsAdmin(NEW.id) != 1 THEN
+    SIGNAL SQLSTATE '50001' SET MESSAGE_TEXT = 'Un étudiant ne peut pas gérer une matière';
+END IF; $$
+delimiter ;
