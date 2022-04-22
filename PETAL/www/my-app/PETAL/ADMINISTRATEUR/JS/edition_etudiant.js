9d6584fd640a6f63fc791b2d71a47dc442b673a3
samlach2222 <lachaudsamuel@protonmail.com>
2022-04-22 16:07:38 +0200

    Modifications visibilité CSS + Ajout du script PHP pour Edition etudiant
    BOOM, aucune caractères restant dans le titre

--- PETAL/www/my-app/PETAL/ADMINISTRATEUR/JS/edition_etudiant.js
+++ PETAL/www/my-app/PETAL/ADMINISTRATEUR/JS/edition_etudiant.js
@@ -1,23 +1,30 @@
 function AjoutImageProfil() {
     var input = document.createElement('input');
     input.type = 'file';
-
     input.onchange = e => {
         // Récuparation de la réference du fichier
         const file = e.target.files[0];
-        if(file && file['type'].split('/')[0] === 'image'){ // si le fichier est une image
-            // Création du lecteur
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
 
-            reader.onload = readerEvent => {
-                const content = readerEvent.target.result;
-                document.querySelector('#ajoutImageProfil').style.backgroundImage = 'url('+ content +')';
-                document.querySelector('#ajoutImageProfil').style.backgroundSize = 'cover';
+        // Check size
+        const fsize = file.size;
+        if (fsize > 8 * 1000000) { // 8 Mb max size
+            window.alert('Veuillez utiliser des images de moins de 8Mo');
+        } else {
+            if(file && file['type'].split('/')[0] === 'image'){ // si le fichier est une image
+                // Création du lecteur
+                const reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = readerEvent => {
+                    const content = readerEvent.target.result;
+                    document.querySelector('#ajoutImageProfil').style.backgroundImage = 'url('+ content +')';
+                    document.querySelector('#ajoutImageProfil').style.backgroundSize = 'cover';
+                    const b64WithPrefix = reader.result;
+                    document.querySelector('#b64Image').value = b64WithPrefix.substring(b64WithPrefix.indexOf(',') + 1);
+                }
             }
-        }
-        else {
-            window.alert("Veuillez choisir un fichier valide");
+            else {
+                window.alert("Veuillez choisir un fichier valide");
+            }
         }
     }
     input.click();
