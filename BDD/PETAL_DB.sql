3939540b7117fdeb1ed342246073fd0f24112fcc
samlach2222 <lachaudsamuel@protonmail.com>
2022-04-22 15:19:21 +0200

    Revert "Changements sur la table Utilisateur pour plus de practicité"
    This reverts commit 5067b3449a99de059bbdc9b316f09bc97b437435.

--- BDD/PETAL_DB.sql
+++ BDD/PETAL_DB.sql
@@ -19,15 +19,14 @@ DROP TABLE IF EXISTS ReponseDeEtudiant;
 
 -- Creation des tables
 CREATE TABLE Utilisateur (
-    num INT NOT NULL,
-    admin BOOLEAN NOT NULL DEFAULT false,
+    id INT NOT NULL AUTO_INCREMENT,
     photoProfil LONGBLOB,
     nom VARCHAR(50) NOT NULL,
     prenom VARCHAR(50) NOT NULL,
     adresseMail VARCHAR(75) NOT NULL,
     numeroTelephone VARCHAR(15),
     motDePasse VARCHAR(100) NOT NULL,
-    PRIMARY KEY (num)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE Administrateur (
@@ -49,7 +48,7 @@ CREATE TABLE Matiere (
     image LONGBLOB,
     id INT NOT NULL,
     PRIMARY KEY(nomMatiere),
-    FOREIGN KEY(id) REFERENCES Utilisateur(num) ON DELETE CASCADE
+    FOREIGN KEY(id) REFERENCES Utilisateur(id) ON DELETE CASCADE
 );
 
 CREATE TABLE MoyenneEtuMatiere (
@@ -64,10 +63,9 @@ CREATE TABLE MoyenneEtuMatiere (
 CREATE TABLE SujetForum (
     nomSujet VARCHAR(50) NOT NULL,
     nomMatiere VARCHAR(50) NOT NULL,
-    id INT NOT NULL,
-    PRIMARY KEY (idSujetForum),
-    FOREIGN KEY (nomMatiere) REFERENCES Matiere(nomMatiere) ON DELETE CASCADE,
-    FOREIGN KEY (id) REFERENCES Utilisateur(num) ON DELETE CASCADE
+    resolu BOOLEAN NOT NULL DEFAULT false,
+    PRIMARY KEY (nomSujet),
+    FOREIGN KEY (nomMatiere) REFERENCES Matiere(nomMatiere) ON DELETE CASCADE
 );
 
 CREATE TABLE MessageForum (
@@ -77,8 +75,8 @@ CREATE TABLE MessageForum (
     nomSujet VARCHAR(50) NOT NULL,
     id INT NOT NULL,
     PRIMARY KEY (idMessage),
-    FOREIGN KEY (idSujetForum) REFERENCES SujetForum(idSujetForum) ON DELETE CASCADE,
-    FOREIGN KEY (id) REFERENCES Utilisateur(num) ON DELETE CASCADE
+    FOREIGN KEY (nomSujet) REFERENCES SujetForum(nomSujet) ON DELETE CASCADE,
+    FOREIGN KEY (id) REFERENCES Utilisateur(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Cours (
@@ -103,12 +101,12 @@ CREATE TABLE QCM (
 );
 
 CREATE TABLE ResultatEtudiant (
-    id INT NOT NULL,
-    idQCM INT NOT NULL,
-    noteExamen DECIMAL(4,2),
-    PRIMARY KEY(id, idQCM),
-    FOREIGN KEY(id) REFERENCES Utilisateur(num) ON DELETE CASCADE,
-    FOREIGN KEY(idQCM) REFERENCES QCM(idQCM) ON DELETE CASCADE
+    id INT NOT NULL AUTO_INCREMENT,
+    nomQCM VARCHAR(50) NOT NULL,
+    noteExam DECIMAL(4,2),
+    PRIMARY KEY(id, nomQCM),
+    FOREIGN KEY(id) REFERENCES Utilisateur(id) ON DELETE CASCADE,
+    FOREIGN KEY(nomQCM) REFERENCES QCM(nomQCM) ON DELETE CASCADE
 );
 
 CREATE TABLE Question (
@@ -127,47 +125,6 @@ CREATE TABLE ReponseDeEtudiant (
     reponseChoisie VARCHAR(7),
     reponseJuste BOOLEAN,
     PRIMARY KEY (id, idQuestion),
-    FOREIGN KEY (id) REFERENCES Utilisateur(num) ON DELETE CASCADE,
+    FOREIGN KEY (id) REFERENCES Etudiant(id) ON DELETE CASCADE,
     FOREIGN KEY (idQuestion) REFERENCES Question(idQuestion) ON DELETE CASCADE
 );
-
-CREATE VIEW MoyenneEtuMatiere AS
-    SELECT ResultatEtudiant.id, nom, prenom, ROUND(SUM(noteExamen)/COUNT(noteExamen), 2) AS moyenne, nomMatiere
-    FROM QCM NATURAL JOIN ResultatEtudiant LEFT JOIN Utilisateur ON ResultatEtudiant.id = Utilisateur.num
-    GROUP BY id;
-
-CREATE VIEW MoyenneQCM AS
-    SELECT idQCM, nomQCM, ROUND(SUM(noteExamen)/COUNT(noteExamen), 2) AS moyenne, nomMatiere
-    FROM QCM NATURAL JOIN ResultatEtudiant
-    GROUP BY idQCM;
-
-
--- Initialisation de la fonction IsAdmin
-DROP FUNCTION IF EXISTS IsAdmin;
-DELIMITER $$
-CREATE FUNCTION IsAdmin(p_id INT) RETURNS tinyint(1)
-BEGIN
-    RETURN (SELECT admin FROM Utilisateur WHERE num = p_id);
-END$$
-DELIMITER ;
-
--- Creation des triggers pour vérifier que l'utilisateur est ou n'est pas un admin
-DROP TRIGGER IF EXISTS trigger_matiere_admin_insert;
-delimiter $$
-CREATE TRIGGER trigger_matiere_admin_insert BEFORE INSERT
-ON matiere
-FOR EACH ROW
-IF IsAdmin(NEW.id) != 1 THEN
-    SIGNAL SQLSTATE '50001' SET MESSAGE_TEXT = 'Un étudiant ne peut pas gérer une matière';
-END IF; $$
-delimiter ;
-
-DROP TRIGGER IF EXISTS trigger_matiere_admin_insert;
-delimiter $$
-CREATE TRIGGER trigger_matiere_admin_insert BEFORE UPDATE
-ON matiere
-FOR EACH ROW
-IF IsAdmin(NEW.id) != 1 THEN
-    SIGNAL SQLSTATE '50001' SET MESSAGE_TEXT = 'Un étudiant ne peut pas gérer une matière';
-END IF; $$
-delimiter ;
