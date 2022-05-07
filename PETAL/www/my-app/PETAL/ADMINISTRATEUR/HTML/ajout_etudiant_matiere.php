962bed348253b3a44077098402d5bcfafd1455ce
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-05-07 02:27:02 +0200

    ajout_etudiant_matiere fonctionnelle

--- PETAL/www/my-app/PETAL/ADMINISTRATEUR/HTML/ajout_etudiant_matiere.php
+++ PETAL/www/my-app/PETAL/ADMINISTRATEUR/HTML/ajout_etudiant_matiere.php
@@ -3,27 +3,39 @@
 <head>
     <?php
         if($_COOKIE['theme'] == 0) { //light
-            echo '<link rel="stylesheet" href="../CSS/resultat_qcm_light.css">';
+            echo '<link rel="stylesheet" href="../CSS/ajout_etudiant_matiere_light.css">';
         }
         else { //dark
-            echo '<link rel="stylesheet" href="../CSS/resultat_qcm_dark.css">';
+            echo '<link rel="stylesheet" href="../CSS/ajout_etudiant_matiere_dark.css">';
         }
     ?>
     <link rel="icon" type="image/x-icon" href="../../Ressources/Icon/logo%20PETAL.svg">
     <meta charset="UTF-8">
-    <title>Ajout d'étudiants</title>
+    <title>Ajout d'étudiants à <?php echo $_GET['matiere']; ?></title>
 </head>
 <body>
-    <?php include("../../ALL/HTML/bandeau.php");
-    include("../PHP/script_etumatiere.php");?>
+    <?php
+        include("../PHP/script_ajout_etudiant_matiere.php");
+        RedirectGestionMatiere();
+        Valider();
+        include("../../ALL/HTML/bandeau.php");
+    ?>
     <div id="corps">
     	<div id="titre">
-    		<h1>Ajout d'étudiants</h1>
+    		<h1>Ajout d'étudiants à <?php echo $_GET['matiere']; ?></h1>
     	</div>
-        <div id="conteneurRetour"><a href="liste_qcm.php" id="retour">retour</a></div>
-        <div id="tableaux">
-    	   <?php AfficheListeEtudiant(); ?>
+        <div>
+            <a href="gestion_matiere.php" id="retour">retour</a>
         </div>
+        <form id="etudiants" method="POST">
+            <div id="liste-etudiants">
+                <?php
+                    ImageMatiere();
+                    AfficheListeEtudiant();
+                ?>
+            </div>
+            <?php BoutonValider(); ?>
+        </form>
     </div>
 </body>
 </html>
--- PETAL/www/my-app/PETAL/ADMINISTRATEUR/PHP/script_ajout_etudiant_matiere.php
+++ PETAL/www/my-app/PETAL/ADMINISTRATEUR/PHP/script_ajout_etudiant_matiere.php
@@ -1,78 +1,95 @@
 <?php
-    // Reception de l'ID de modification
-    if (isset($_POST['data'])) {
-        $idQCM = json_decode($_POST['data']);
+    // Initialisation connexion BDD
+    include_once('../../ALL/PHP/BDD.php');
 
-        // Initialisation connexion BDD
-        $dsn = "mysql:host=localhost;dbname=petal_db;charset=UTF8";
-        try {
-            $pdo = new PDO($dsn, "root", "root");
+    function RedirectGestionMatiere() {
+        //Si la matiere n'est pas précisé
+        if (empty($_GET['matiere'])) {
+            header("location: gestion_matiere.php");
+            exit;
         }
-        catch (PDOException $e) {
-            echo $e->getMessage();
+        
+        //Reprend la session
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
         }
-        // Requete de récupération 
-        $idQCM=$_GET['id'];
+        
+        global $pdo;
+        
+        //Essaye de récupèrer l'image de la matière et vérifie que l'admin connecté y a accès
+        $prepared = $pdo->prepare("SELECT image FROM matiere WHERE num = :num && nomMatiere = :nomMatiere", array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $prepared->execute(array('num' => $_SESSION['num'], 'nomMatiere' => $_GET['matiere']));
+        $rows = $prepared->fetchAll();
+        
+        if (count($rows) == 0) {
+            header("location: gestion_matiere.php");
+            exit;
+        }
+        
+        global $imageMatiere;
+        $imageMatiere = $rows[0][0];
+    }
+
+    function ImageMatiere() {
+        global $imageMatiere;
+        
+        //Si l'image n'est pas null
+        if ($imageMatiere) {
+            echo '<div id="image-matiere" style="background-image: url(\'data:image;base64,'.base64_encode($imageMatiere).'")></div>';
+        }
     }
 
     function AfficheListeEtudiant() {
-        // Initialisation connexion BDD
-        $dsn = "mysql:host=localhost;dbname=petal_db;charset=UTF8";
-        try {
-            $pdo = new PDO($dsn, "root", "root");
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
-        $idQCM=$_GET['id'];
-        $query = "SELECT prenom, nom, num FROM utilisateur WHERE num = " . $row[0];
-        foreach ($pdo->query($query) as $row2) {
-            echo "<tr class=\"note\"><td>".strtoupper($row2[1])." ".ucfirst(strtolower($row2[0]))."</td>";
+        
+        global $pdo;
+        //La quatrième colonne permet de pré-cocher les étudiants ayant actuellement accès à la matière
+        $rows = $pdo->query("SELECT utilisateur.num, nom, prenom, IF(!ISNULL(tableEtuMatiere.num), 'checked', null) AS dejaAjoute FROM utilisateur LEFT JOIN (SELECT num FROM etumatiere WHERE nomMatiere = '3') AS tableEtuMatiere ON utilisateur.num = tableEtuMatiere.num WHERE admin = 0");
+        
+        global $aucunEtudiant;
+        $aucunEtudiant = count($rows) == 0;
+        
+        if (!$aucunEtudiant) {
+            $index = 0;
+            foreach ($rows as $row) {
+                echo '<li>
+                    <input type="checkbox" class="CB" name="cb'.$index.'" value="'.$row[0].'" '.$row[3].'/>
+                    <span class="nom">'.ucfirst(mb_strtolower($row[2], 'UTF-8')).' '.mb_strtoupper($row[1], 'UTF-8').'</span>
+                </li>';
+                $index++;
+            }
+            echo '<input type="hidden" name="nbEtudiants" value="'.$index.'"/>';
+        } else {
+            echo '<span id="aucun-etudiant">Aucun étudiant existant</span>';
         }
     }
-    function AfficheListeEtudiant() {
-        // Initialisation connexion BDD
-        $dsn = "mysql:host=localhost;dbname=petal_db;charset=UTF8";
-        try {
-            $pdo = new PDO($dsn, "root", "root");
-        } catch (PDOException $e) {
-            echo $e->getMessage();
+
+    function BoutonValider() {
+        global $aucunEtudiant;
+        
+        if (!$aucunEtudiant) {
+            echo '<button id="valider" type="submit">Valider</button>';
         }
-        $nomMatiere=$_GET['matiere'];
-        $liste=array();
-        $query = "SELECT num, nomMatiere FROM etumatiere WHERE nomMatiere = " . $nomMatiere;
-        foreach ($pdo->query($query) as $row) {
-            array_push($liste, $row[0]);
-        }
-        // Requete de récupération de tout les qcm
-        $query = "SELECT prenom, nom, num from utilisateur";
-        reset($liste);
-        $comp=1;
-        foreach ($pdo->query($query) as $row) {
-            if ($row[2]==current($liste)) {
-                echo "
-                <li>
-                    <label>
-                        <input type=\"hidden\" name=\"numEtu\" value=\"" . $row[2] . "\" id=\"numEtu\"/>
-                        <input type=\"checkbox\" class=\"CB\" select=\"selected\" name=\"etu\" value=\"value\"/>
-                        <span>" .ucfirst(strtolower($row[0]))." ".strtoupper($row[1]). "</span>
-                    </label>
-                </li>";
-                if ($comp!=count($liste)) {
-                    next($liste);
-                    $comp=$comp+1;
-                }
+    }
+
+    function Valider() {
+        if (!empty($_POST['nbEtudiants'])) {
+            global $pdo;
+            
+            //Supprime d'abord tous les étudiants ayant accès à la matière...
+            $prepared = $pdo->prepare("DELETE FROM etumatiere WHERE nomMatiere = :nomMatiere", array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+            $prepared->execute(array('nomMatiere' => $_GET['matiere']));
+            
+            
+            //...Puis insére tous les étudiants selectionnés
+            $prepared = $pdo->prepare("INSERT INTO etumatiere VALUES (:num, :nomMatiere)", array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+            for ($i = 0; $i < $_POST['nbEtudiants']; $i++) {
+                if (isset($_POST['cb'.$i])) {
+                    $prepared->execute(array('num' => $_POST['cb'.$i], 'nomMatiere' => $_GET['matiere']));
+                } 
             }
-            else
-            {
-                echo "
-                <li>
-                    <label>
-                        <input type=\"hidden\" name=\"numEtu\" value=\"" . $row[2] . "\" id=\"numEtu\"/>
-                        <input type=\"checkbox\" class=\"CB\"  name=\"etu\" value=\"value\"/>
-                        <span>" .ucfirst(strtolower($row[0]))." ".strtoupper($row[1]). "</span>
-                    </label>
-                </li>";
-            }
+            
+            //Permet de ne pas ré-envoyer le POST si l'utilisateur rafraichie la page
+            header("Refresh:0");
         }
     }
 ?>
